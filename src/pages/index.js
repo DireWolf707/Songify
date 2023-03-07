bc3f5e572192f9e22db510439b8f651754939c4f
DireWolf707 <rahulsood707@gmail.com>
2023-03-07 17:38:37 +0530

    added empty pages components

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import TopArtists from "./TopArtists"
+import TopCharts from "./TopCharts"
+import Search from "./Search"
+import Home from "./Home"
+import AroundYou from "./AroundYou"
+
+export { TopArtists, TopCharts, Search, Home, AroundYou }
