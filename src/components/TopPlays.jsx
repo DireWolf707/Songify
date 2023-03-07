7c7017d91f73835990f075a8934a9112a32ac35f
DireWolf707 <rahulsood707@gmail.com>
2023-03-07 17:38:57 +0530

    added Top plays component

---
+++ src/components/TopPlays.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { Stack } from "@mui/material"
+
+const TopPlays = () => {
+  return (
+    <Stack flexShrink={0} sx={{ width: { md: "100%", lg: "440px" },  }}>
+      TopPlays
+    </Stack>
+  )
+}
+
+export default TopPlays
