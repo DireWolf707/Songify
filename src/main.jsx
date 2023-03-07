4f6b29198b11cf39a54a6dd77a9b15de0759df1a
DireWolf707 <rahulsood707@gmail.com>
2023-03-07 17:37:56 +0530

    added ThemeProvider and Router

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,15 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./index.css"
+import { BrowserRouter as Router } from "react-router-dom"
+import { createTheme, ThemeProvider } from "@mui/material"
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />)
+const theme = createTheme({})
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ThemeProvider theme={theme}>
+    <Router>
+      <App />
+    </Router>
+  </ThemeProvider>
+)
