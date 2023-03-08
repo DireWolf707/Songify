df15da508765802c4cd0ce14b9cb7332459be463
DireWolf707 <rahulsood707@gmail.com>
2023-03-08 14:20:55 +0530

    added Search page

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,9 +1,28 @@
 import React from "react"
 import { useParams } from "react-router-dom"
+import { useSearchSongsQuery } from "../store"
+import { Typography, Stack } from "@mui/material"
+import Feed from "../components/feeds/Feed"
+import SearchBar from "../components/SearchBar"
 
 const Search = () => {
   const { searchTerm } = useParams()
-  return <div>{searchTerm}</div>
+  const { data, isFetching } = useSearchSongsQuery(searchTerm)
+
+  return (
+    <Stack flexGrow={1} sx={{ overflow: "auto" }}>
+      <SearchBar />
+      <Stack flexDirection="row" p="24px" gap={2}>
+        <Typography variant="h4" fontWeight="bold">
+          Results For :
+        </Typography>
+        <Typography variant="h4" fontWeight="bold" sx={{ color: "#007FFF" }}>
+          {searchTerm}
+        </Typography>
+      </Stack>
+      <Feed songs={data} isFetching={isFetching} />
+    </Stack>
+  )
 }
 
 export default Search
