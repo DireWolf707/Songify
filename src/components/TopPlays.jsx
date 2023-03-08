0f49606950cb1d90d5181ceb8f66f599f6973c34
DireWolf707 <rahulsood707@gmail.com>
2023-03-08 14:21:49 +0530

    added Top plays component

--- src/components/TopPlays.jsx
+++ src/components/TopPlays.jsx
@@ -1,10 +1,18 @@
 import React from "react"
 import { Stack } from "@mui/material"
+import { Typography } from "@mui/material"
+import { useFetchTopArtistsQuery } from "../store"
+import TopArtistsFeed from "./feeds/TopArtistsFeed"
 
 const TopPlays = () => {
+  const { data, isFetching } = useFetchTopArtistsQuery()
+
   return (
-    <Stack flexShrink={0} sx={{ width: { md: "100%", lg: "440px" },  }}>
-      TopPlays
+    <Stack flexShrink={0} sx={{ width: { md: "100%", lg: "440px" }, overflow: "auto" }}>
+      <Typography variant="h5" fontWeight="bold" p="12px">
+        Top 10 Artists
+      </Typography>
+      <TopArtistsFeed artists={data} isFetching={isFetching} limit={10} />
     </Stack>
   )
 }
