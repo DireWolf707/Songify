2e11cbb6b45e44ae241e4035f98fd1d80bec76a6
DireWolf707 <rahulsood707@gmail.com>
2023-03-08 14:19:00 +0530

    added store

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,17 @@ import App from "./App"
 import "./index.css"
 import { BrowserRouter as Router } from "react-router-dom"
 import { createTheme, ThemeProvider } from "@mui/material"
+import { Provider as StoreProvider } from "react-redux"
+import { store } from "./store"
 
 const theme = createTheme({})
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <ThemeProvider theme={theme}>
-    <Router>
-      <App />
-    </Router>
-  </ThemeProvider>
+  <StoreProvider store={store}>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <App />
+      </Router>
+    </ThemeProvider>
+  </StoreProvider>
 )
