439ebdb01e12ac1093efd969a268b1efbbcde998
DireWolf707 <rahulsood707@gmail.com>
2023-03-08 14:20:35 +0530

    updated sidebar for store

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,13 +2,17 @@ import React, { useEffect } from "react"
 import { useMediaQuery, Box, Drawer, List, ListItemButton, ListItemIcon, ListItemText, ListItem, Divider } from "@mui/material"
 import { links } from "../utils/constants"
 import { Link, useLocation } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
+import { toggleSidebar } from "../store"
 
-const SideBar = ({ open, setOpen }) => {
+const SideBar = () => {
   const location = useLocation()
+  const dispatch = useDispatch()
+  const { sidebar: open } = useSelector((store) => store.data)
   const isSmall = useMediaQuery((theme) => theme.breakpoints.only("xs"))
 
   useEffect(() => {
-    if (!isSmall && open) setOpen(false)
+    if (!isSmall && open) dispatch(toggleSidebar(false))
   }, [isSmall])
 
   return (
@@ -16,7 +20,7 @@ const SideBar = ({ open, setOpen }) => {
       anchor="left"
       variant={isSmall ? "temporary" : "permanent"}
       open={open}
-      onClose={() => setOpen(false)}
+      onClose={() => dispatch(toggleSidebar(false))}
       PaperProps={{
         sx: {
           width: "240px",
@@ -33,7 +37,7 @@ const SideBar = ({ open, setOpen }) => {
       // sx={{ overflow: "auto" }}
       >
         {links.map((link, idx) => (
-          <Link key={idx} to={link.to} onClick={() => open && setOpen(false)}>
+          <Link key={idx} to={link.to} onClick={() => open && dispatch(toggleSidebar(false))}>
             <ListItemButton selected={location.pathname === link.to}>
               <ListItemIcon>{link.icon}</ListItemIcon>
               <ListItemText primary={link.name} />
